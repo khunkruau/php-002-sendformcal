f1313a10e39a030664af3ae22357c5885f2a625d
khunkruau <khunkruau@gmail.com>
2023-07-21 10:20:49 +0700

    first commit

---
+++ re.php
@@ -0,0 +1,16 @@
+﻿<html>
+<head>
+	<title>รับข้อมูลฟอร์ม คำนวณ</title>
+</head>
+<body>
+<h4>#หน้ารับข้อมูลฟอร์ม คำนวณ</h4>
+<?php
+	$rfw = $_POST["fw"];
+	$rfh = $_POST["fh"];
+	$area = $rfw*$rfh;
+	echo "กว้าง ".$rfw."<br>";
+	echo "ยาว ".$rfh."<br>";
+	echo "พื้นที่สี่เหลี่ยม เท่ากับ ".$area." ตาราง ซม.";
+?>
+</body>
+</html>
